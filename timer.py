34f3250ae53fbf0cb810870fa09db2361817dfa0
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 13:25:57 -0500

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,8 +1,9 @@
 from browser import document as doc, window as win
 from datetime import time
 sched = eval(doc.getElementById("sched").innerHTML)
-timer = doc.getElementById("time").innerHTML
+timer = doc.getElementById("time")
+timer.innerHTML = "0"
 def get():
-    timer = str(timer.innerHTML)+1)
+    timer = str(int(timer.innerHTML)+1)
 
 win.setInterval(get, 1000)
