757a504b50e5d4dc7bff89744de41615eb55ac25
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 18:37:39 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,1 +1,12 @@
-
+function sched(s) {
+    document.getElementById("sched").innerHTML = document.getElementById(s).innerHTML;
+    document.cookie = "sched="+s;
+}
+function color(s) {
+    document.body.style.color = s;
+    document.cookie = "color="+s;
+}
+function theme(s,v) {
+    document.body.backgroundColor = s;
+    document.cookie = "color="+v;
+}
