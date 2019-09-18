723ba6ad86021ee0adbaddb8a4b6c39c26df1863
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 17:56:04 -0500

    Update sched.py

--- sched.py
+++ sched.py
@@ -2,7 +2,8 @@ from browser import document as doc
 if len(doc.cookie):
     for ck in doc.cookie.split('; '):
         if ck.startswith('sched'):
-            doc.getElementById("sched").innerHTML = doc.getElementById(ck.split('=')[1]).innerHTML.strip()
+            doc.getElementById("sched").innerHTML = doc.getElementById(ck.split('=')[1]).innerHTML
+            doc.write('sched')
         elif ck.startswith('color'):
             doc.body.style.color = ck.split('=')[1]
         elif ck.startswith('theme'):
