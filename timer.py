b7597937f0a0e744861bc66d6edc6d4cd6327527
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 11:28:15 -0500

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,7 +1,9 @@
-from browser import document as doc
+from browser import document as doc, window as win
 from time import sleep
 x = 0
-while True:
-    doc.getElementById("time").innerHTML = str(x)
-    x += 1
-    sleep(1)
+doc.getElementById("time").innerHTML = "0"
+def i():
+    x = int(doc.getElementById("time").innerHTML)
+    doc.getElementById("time").innerHTML = str(x+1)
+
+win.setInterval(i, 1000)
