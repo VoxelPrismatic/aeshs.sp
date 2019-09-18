3d0aa2439cdcb571a009d15b40a2e0fe3657ac52
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 15:16:17 -0500

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,26 +1,29 @@
-from browser import document as doc, window as win
-from datetime import time, timedelta as td, datetime as dt
+try:
+ from browser import document as doc, window as win
+ from datetime import time, timedelta as td, datetime as dt
 
-global sched
-sched = eval(doc.getElementById("sched").innerHTML)
-doc.getElementById("sched").innerHTML = "LOL"
-def elem():
+ global sched
+ sched = eval(doc.getElementById("sched").innerHTML)
+ doc.getElementById("sched").innerHTML = "LOL"
+ def elem():
     return doc.getElementById("time")
-def perm():
+ def perm():
     return doc.getElementById("per")
-elem().innerHTML = "0"
-def diff(t1):
+ elem().innerHTML = "0"
+ def diff(t1):
     t2 = dt.now()
     t1 = dt.combine(dt.today(), t1)
     return (t1-t2).total_seconds()
-def nextP():
+ def nextP():
     for per in sched:
         if diff(sched[per]) >= 0:
             return per, dt.combine(dt.today(), sched[per]), dt.now()
-def zf(itm):
+ def zf(itm):
     return str(itm).zfill(2)
-def get():
+ def get():
     period, end, rn = nextP()
     elem().innerHTML = ':'.join(zf(x) for x in str(end-now).split('.')[0].split(':'))
     perm().innerHTML = f"PERIOD {period} // {zf(end.hour)}:{zf(end.minute)}:{zf(end.second)}"
-win.setInterval(get, 500)
+ win.setInterval(get, 500)
+except Exception as ex:
+    doc.write(ex)
