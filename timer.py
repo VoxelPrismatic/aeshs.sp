b833c908c136f6ab29deac05169e683467e20307
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 13:48:19 -0500

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,9 +1,17 @@
 from browser import document as doc, window as win
-from datetime import time
+from datetime import time, timedelta as td, datetime as dt, combine as cb, now
+global sched
 sched = eval(doc.getElementById("sched").innerHTML)
-timer = doc.getElementById("time")
-timer.innerHTML = "0"
+doc.getElementById("sched").innerHTML = ""
+def elem():
+    return doc.getElementById("time")
+elem().innerHTML = "0"
+def diff(t1, t2):
+    return (t1-dt(1970,1,1)).total_seconds()-(t2-dt(1970,1,1)).total_seconds()
+def nextP():
+    for per in sched:
+        if diff(sched[per],now()) >= 0:
 def get():
-    timer = str(int(timer.innerHTML)+1)
+    elem().innerHTML = str(int(elem().innerHTML)+1)
 
 win.setInterval(get, 1000)
