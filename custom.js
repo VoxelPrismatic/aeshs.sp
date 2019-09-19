bd4a2569ba241c970ee9e3debe04254969414b7c
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 21:10:26 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,6 +1,23 @@
+var type = document.getElementById("typ");
 function sched(s) {
-    document.getElementById("sched").innerHTML = document.getElementById(s).innerHTML;
-    document.cookie = "sched="+s;
+    if (s == "half") {
+        if (document.getElementById("typ").innerHTML.contains("half")) {
+            type.innerHTML = type.innerHTML.replace("_half","");
+            document.getElementById("sched").innerHTML = document.getElementById(type.innerHTML).innerHTML;
+            document.cookie = "sched="+type.innerHTML;
+        } else if (document.getElementById("typ").innerHTML != "late") {
+            type.innerHTML = type.innerHTML.replace("_half","");
+            document.getElementById("sched").innerHTML = document.getElementById(type.innerHTML).innerHTML;
+            document.cookie = "sched="+type.innerHTML;
+        }
+        
+    } else {
+        if (s == "late") {
+            type.innerHTML = type.innerHTML.split("_")[0]
+        }
+        document.getElementById("sched").innerHTML = document.getElementById(s).innerHTML;
+        document.cookie = "sched="+s;
+    }
 }
 function color(s) {
     document.body.style.color = s;
