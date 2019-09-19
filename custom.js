586608e02e193b4a5cc52c8cc871347f35c732a8
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 21:21:57 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,29 +1,17 @@
-var type = document.getElementById("typ");
 function sched(s) {
     if (s == "half") {
-        if (document.getElementById("typ").innerHTML.contains("half")) {
-            type.innerHTML = type.innerHTML.replace("_half","");
-            document.getElementById("sched").innerHTML = document.getElementById(type.innerHTML).innerHTML;
-            document.cookie = "sched="+type.innerHTML;
-        } else if (document.getElementById("typ").innerHTML != "late") {
-            type.innerHTML = type.innerHTML.replace("_half","");
-            document.getElementById("sched").innerHTML = document.getElementById(type.innerHTML).innerHTML;
-            document.cookie = "sched="+type.innerHTML;
+        if (document.getElementById("half").innerHTML == "True") {
+            document.getElementById("half").innerHTML = "False";
+        } else {
+            document.getElementById("half").innerHTML = "True";
         }
-        
     } else {
-        if (s == "late") {
-            type.innerHTML = type.innerHTML.split("_")[0]
-        }
         document.getElementById("sched").innerHTML = document.getElementById(s).innerHTML;
-        document.cookie = "sched="+s;
     }
 }
 function color(s) {
     document.body.style.color = s;
-    document.cookie = "color="+s;
 }
 function theme(s,v) {
     document.body.backgroundColor = s;
-    document.cookie = "color="+v;
 }
