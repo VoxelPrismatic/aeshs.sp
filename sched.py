a0121e4ccc350ee94fe756c0162d9b5e6d084c3b
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-20 11:17:30 -0500

    Update sched.py

--- sched.py
+++ sched.py
@@ -1,31 +1,23 @@
 from browser import document as doc
 if len(doc.cookie):
-    if "color=" not in doc.cookie:
-        doc.cookie = "color=#00ffffff"
-    if "theme=" not in doc.cookie:
-        doc.cookie = "theme=dark"
-    if "sched=" not in doc.cookie:
-        doc.cookie = "sched=norm"
-    if "custom0=" not in doc.cookie:
-        doc.cookie = "custom0="+doc.getElementById("norm").innerHTML
-    if "custom1=" not in doc.cookie:
-        doc.cookie = "custom1="+doc.getElementById("norm").innerHTML
-    if "custom2=" not in doc.cookie:
-        doc.cookie = "custom2="+doc.getElementById("norm").innerHTML
-    if "custom3=" not in doc.cookie:
-        doc.cookie = "custom3="+doc.getElementById("norm").innerHTML
-    if "custom4=" not in doc.cookie:
-        doc.cookie = "custom4="+doc.getElementById("norm").innerHTML
-    if "custom5=" not in doc.cookie:
-        doc.cookie = "custom5="+doc.getElementById("norm").innerHTML
-    if "custom6=" not in doc.cookie:
-        doc.cookie = "custom6="+doc.getElementById("norm").innerHTML
-    if "custom7=" not in doc.cookie:
-        doc.cookie = "custom7="+doc.getElementById("norm").innerHTML
-    if "custom8=" not in doc.cookie:
-        doc.cookie = "custom8="+doc.getElementById("norm").innerHTML
-    if "custom9=" not in doc.cookie:
-        doc.cookie = "custom9="+doc.getElementById("norm").innerHTML
+    norm_sched = doc.getElementById("norm").innerHTML
+    default = {"color=": "#00ffffff",
+               "theme=": "dark",
+               "sched=": "norm",
+               "custom0=": norm_sched,
+               "custom1=": norm_sched,
+               "custom2=": norm_sched,
+               "custom3=": norm_sched,
+               "custom4=": norm_sched,
+               "custom5=": norm_sched,
+               "custom6=": norm_sched,
+               "custom7=": norm_sched,
+               "custom8=": norm_sched,
+               "custom9=": norm_sched
+              }
+    for key in default:
+        if key not in doc.cookie:
+            doc.cookie = key+default[key]
     for ck in doc.cookie.split(';'):
         ck = ck.strip()
         if ck.startswith('sched'):
@@ -48,41 +40,24 @@ if len(doc.cookie):
                 doc.getElementById("theme").innerHTML = "light"
                 doc.getElementById("change").innerHTML = "[DARK THEME]"
                 doc.getElementById("change").style.color = "#112222ff"
-        elif ck.startswith('custom0'):
-            doc.getElementById("custom0").innerHTML = ck[7:]
-        elif ck.startswith('custom1'):
-            doc.getElementById("custom1").innerHTML = ck[7:]
-        elif ck.startswith('custom2'):
-            doc.getElementById("custom2").innerHTML = ck[7:]
-        elif ck.startswith('custom3'):
-            doc.getElementById("custom3").innerHTML = ck[7:]
-        elif ck.startswith('custom4'):
-            doc.getElementById("custom4").innerHTML = ck[7:]
-        elif ck.startswith('custom5'):
-            doc.getElementById("custom5").innerHTML = ck[7:]
-        elif ck.startswith('custom6'):
-            doc.getElementById("custom6").innerHTML = ck[7:]
-        elif ck.startswith('custom7'):
-            doc.getElementById("custom7").innerHTML = ck[7:]
-        elif ck.startswith('custom8'):
-            doc.getElementById("custom8").innerHTML = ck[7:]
-        elif ck.startswith('custom9'):
-            doc.getElementById("custom9").innerHTML = ck[7:]
+        elif ck.startswith('custom'):
+            doc.getElementById(ck.split('=')[0]).innerHTML = ck[8:]
           
 else:
     doc.cookie="color=#00ffffff"
     doc.cookie="theme=dark"
     doc.cookie="sched=norm"
-    doc.cookie = "custom0="+doc.getElementById("norm").innerHTML
-    doc.cookie = "custom1="+doc.getElementById("norm").innerHTML
-    doc.cookie = "custom2="+doc.getElementById("norm").innerHTML
-    doc.cookie = "custom3="+doc.getElementById("norm").innerHTML
-    doc.cookie = "custom4="+doc.getElementById("norm").innerHTML
-    doc.cookie = "custom5="+doc.getElementById("norm").innerHTML
-    doc.cookie = "custom6="+doc.getElementById("norm").innerHTML
-    doc.cookie = "custom7="+doc.getElementById("norm").innerHTML
-    doc.cookie = "custom8="+doc.getElementById("norm").innerHTML
-    doc.cookie = "custom9="+doc.getElementById("norm").innerHTML
+    norm_sched = doc.getElementById("norm").innerHTML
+    doc.cookie = "custom0="+norm_sched
+    doc.cookie = "custom1="+norm_sched
+    doc.cookie = "custom2="+norm_sched
+    doc.cookie = "custom3="+norm_sched
+    doc.cookie = "custom4="+norm_sched
+    doc.cookie = "custom5="+norm_sched
+    doc.cookie = "custom6="+norm_sched
+    doc.cookie = "custom7="+norm_sched
+    doc.cookie = "custom8="+norm_sched
+    doc.cookie = "custom9="+norm_sched
     doc.getElementById("sched").innerHTML = doc.getElementById("norm").innerHTML
 doc.getElementById("time").innerHTML = "-~-"
 doc.getElementById("cookie").innerHTML = doc.cookie
