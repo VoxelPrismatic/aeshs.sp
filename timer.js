a3d6fb51d5313e7006ca587b20cebaec8aae6b06
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-01-16 12:02:10 -0600

    Update timer.js

--- timer.js
+++ timer.js
@@ -24,10 +24,8 @@ function diff(t1) {
 function diffTime(a, b) {
     var tA = a.getSeconds() + 60 * a.getMinutes() + 60 * 60 + a.getHours();
     var tB = b.getSeconds() + 60 * b.getMinutes() + 60 * 60 + b.getHours();
-    var d1 = Math.abs(tA - tB);
-    var d2 = Math.abs(tB - tA);
-    console.log("A - B: " + d1 + "\nB - A: " + d2);
-    return d1;
+    console.log(tA, " ", tB);
+    return Math.max(tA, tB) - Math.min(tA, tB);
 }
 
 function zf(itm) {
