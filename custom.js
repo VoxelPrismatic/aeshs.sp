8d7c73adef2e85c4f82b01d8ef01b9bce503c33d
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2020-03-18 13:28:05 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -38,35 +38,45 @@ function sched(s) {
     }
     get();
 }
+function nextYear() {
+    var inAYear = Date.now();
+    inAYear += 60 * 60 * 24 * 365 * 1000;
+    inAYear = Date(inAYear);
+    return inAYear.toString();
+}
 function color(s) {
     s = s.trim();
     gID("colorswap").value = s;
+    if(s == "")
+        return;
     var re = [
         /^#[A-Fa-f0-9]{3}$/gm,
         /^#[A-Fa-f0-9]{4}$/gm,
         /^#[A-Fa-f0-9]{6}$/gm,
-        /^#[A-Fa-f0-9]{8}$/gm
+        /^#[A-Fa-f0-9]{8}$/gm,
+        /^rgb\((\d+(\.\d+)?,?){3}\)/gm,
+        /^rgba\((\d+(\.\d+)?,?){4}\)/gm,
+        /^hsl\((\d+(\.\d+|%)?,?){3}\)/gm
     ];
-    if(s == "")
-        return;
     for(var r of re) {
         if(s.replace(r, "") == "") {
             document.body.style.color = s;
             gEDIT("color", s);
-            document.cookie=`color=${s}; expires=${gHTML("date")}`;
+            document.cookie = `color=${s}`;
+            document.cookie = `expires=${nextYear()}`;
             gSTYLE("customizer").color = s;
         }
     }
 }
 function theme(s) {
-    document.body.backgroundColor = s;
     if (s == "#112222ff") {
         document.body.style.backgroundColor = "#112222ff";
         gSTYLE("change").color = "#ccddddff";
         gID("change").onclick = function(){theme('#ccddddff');};
         gEDIT("change", "[LIGHT THEME]");
         gEDIT("theme", "dark");
-        document.cookie = `theme=dark; expires=${gHTML("date")}`;
+        document.cookie = 'theme=dark;'
+        document.cookie = `expires=${nextYear()}`;
         gSTYLE("customizer").backgroundColor = "#223333ff";
     } else {
         document.body.style.backgroundColor = "#ccddddff";
@@ -74,7 +84,8 @@ function theme(s) {
         gID("change").onclick = function(){theme('#112222ff');};
         gEDIT("change", "[DARK THEME]");
         gEDIT("theme", "light");
-        document.cookie = `theme=light; expires=${gHTML("date")}`;
+        document.cookie = 'theme=light;'
+        document.cookie = `expires=${nextYear()}`;
         gSTYLE("customizer").backgroundColor = "#bbccccff";
     }
 }
@@ -85,11 +96,15 @@ function wheme() {
         gSTYLE("change").color = "#112222ff";
         gID("change").onclick = function(){theme('#112222ff');};
         gEDIT("change", "[DARK THEME]");
+        document.cookie = 'theme=light;'
+        document.cookie = `expires=${nextYear()}`;
     } else {
         document.body.style.backgroundColor = "#112222ff";
         gSTYLE("change").color = "#ccddddff";
         gID("change").onclick = function(){theme('#ccddddff');};
         gEDIT("change","[LIGHT THEME]");
+        document.cookie = 'theme=dark;'
+        document.cookie = `expires=${nextYear()}`;
     }
 }
 
@@ -117,6 +132,6 @@ function textareaTheme() {
 function changer(str, itm) {
     gEDIT(str, gID("customizer").value);
     gEDIT("sched", gID("customizer").value);
-    document.cookie="expires="+gHTML("date");
+    document.cookie="expires="+nextYear();
     document.cookie="path=/";
 }
