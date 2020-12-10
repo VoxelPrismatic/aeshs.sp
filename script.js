fef8112fd80c9fa1d1556dd19eaf8f22e7848d98
VoxelPrismatic <danielpetrov@outlook.com>
2020-12-10 09:33:58 -0600

    more silly bug fixing

--- script.js
+++ script.js
@@ -643,10 +643,13 @@ function get() {
     var now = Date.now()
     var d = new Date()
     var scs = diffTime(d, last_end);
-    if(last_time && scs % 60 != 59) {
-        last_time = scs
-        $("#seconds").textContent = zf(scs % 60)
-        return
+    last_time = scs
+    scs %= 60
+    if(last_time && scs != 59) {
+        if(Number($("#seconds").textContent) > scs) {
+            $("#seconds").textContent = zf(scs)
+            return
+        }
     }
     if(d.getMonth() != just_now.getMonth() || d.getDate() != just_now.getDate() ||
        d.getYear() != just_now.getYear() || d.getDay() != just_now.getDay())
